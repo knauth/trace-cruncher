ceab58efea01b6f0e6bf2c223ad6568b818cd490
June Knauth <june.knauth@gmail.com>
2022-06-15 13:58:32 -0700

    Update install.sh; remove sudo from prefixes

--- install.sh
+++ install.sh
@@ -5,37 +5,37 @@
 
 # Install Dependencies
 echo "---Installing APT Deps---"
-sudo apt update
-sudo apt install build-essential git cmake libjson-c-dev libpython3-dev cython3 python3-numpy python3-pip flex valgrind binutils-dev pkg-config swig -y
-sudo pip3 install --system pkgconfig GitPython
+apt update
+apt install build-essential git cmake libjson-c-dev libpython3-dev cython3 python3-numpy python3-pip flex valgrind binutils-dev pkg-config swig -y
+pip3 install --system pkgconfig GitPython
 
 echo "---Building Other Deps---"
 git clone https://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git/
 cd libtraceevent
 make
-sudo make install
+make install
 cd ..
 git clone https://git.kernel.org/pub/scm/libs/libtrace/libtracefs.git/
 cd libtracefs
 make
-sudo make install
+make install
 cd ..
 git clone https://git.kernel.org/pub/scm/utils/trace-cmd/trace-cmd.git
 cd trace-cmd
 make
-sudo make install_libs
+make install_libs
 cd ..
 git clone https://github.com/yordan-karadzhov/kernel-shark-v2.beta.git kernel-shark
 cd kernel-shark/build
 cmake ..
 make
-sudo make install
+make install
 cd ../..
 
 echo "---Installing trace-cruncher---"
 make
-sudo make install
+make install
 
 echo "---Running Unit Tests---"
 cd tests
-sudo python3 -m unittest discover .
+python3 -m unittest discover .
